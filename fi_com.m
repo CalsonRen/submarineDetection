9937ebc348580e80f49af2a7f930624d7e704310
renjie <calsonren@gmail.com>
2017-01-11 23:21:34 -0500

    Revert "submarine detection source code"
    This reverts commit 36adf5c724a9992399b1b5102852ac51df0da00b.

--- fi_com.m
+++
@@ -1,7 +0,0 @@
-%influence of polarization
-%decide fi=0, theta=60
-%compare the scene of 10-00, 10-05
-data1=load('seawave/10-00.shp');
-for test = 0:45:180
-    sim_3min(data1,0.5,0.5,test,30,'fi_com');
-end
